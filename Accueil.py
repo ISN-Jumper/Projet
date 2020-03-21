689e1fd019825ecc3e729727a1cb947ca941c556
Jeff <jeffreyson18@gmail.com>
2020-03-21 20:25:53 +0100

    Test1

--- Accueil.py
+++ Accueil.py
@@ -3,31 +3,38 @@ from tkinter import font
 import os
 
 def ChangePage():
-    os.startfile("C:/Users/remi1/PycharmProjects/SpaceJumper3000/PartieJeu.py")
+    os.startfile("PartieJeu.py")
     window.quit()
 
 if __name__ == '__main__':
-#pppppppppppphjzhkek
+#pourrr teesssssteeeeeer
     window=Tk()
     window.title("SPACE JUMPER 3000")
-    window.geometry("1280x720+40+50")
+
+    #Faire en sorte que la taille de la fenêtre s'adapte à n'importe quel écran :
+    largeur_fenetre = window.winfo_screenwidth()
+    hauteur_fenetre = window.winfo_screenheight()
+
+    window.geometry("%dx%d+0+0" % (largeur_fenetre,hauteur_fenetre))
+    #window.geometry("1280x720+40+50")
 
     #------- Police -------#
 
     Times = font.Font(family='Times', size=16, weight='bold')
 
-    Ciel=PhotoImage(file='C:/Users/remi1/Desktop/SpaceJUMP/Ciel.png')
+    Ciel=PhotoImage(file='CielAccueil.png')
 
     #------------- Graphique accueil --------#
 
-    Wplan=Canvas(window, width=1280, height=720)
+    Wplan=Canvas(window, width=largeur_fenetre, height=hauteur_fenetre)
     Wplan.grid()
     Wplan.create_image(640, 360, image=Ciel)
 
-#hjklmhjkl
 
     Jouer=Button(window, text='Jouer', width=5, height=1, font =Times, command = lambda : ChangePage())
     Jouer.grid()
-    Jouer.place(x=400, y=560)
+    Jouer.place(x=(largeur_fenetre//2), y=(hauteur_fenetre//1.5))
+
+    Wquitter = Button(window, text="Quitter", command=window.quit).grid()
 
     window.mainloop()
--- PartieJeu.py
+++ PartieJeu.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import font
 import random
 import time
+from timeit import default_timer
 
 
 def Haut(evt):
@@ -52,83 +53,89 @@ def Droite(evt):
 def Attaque():
     global AsteroX
     global AsteroY
-    global AsteroX1
-    global AsteroY1
-    global AsteroX2
-    global AsteroY2
     global Deltax
     global NumImage
     global mesImages
     global Wplan
     global Wasteroide
-    global Wasteroide1
-    global Wasteroide2
 
     Wplan.delete(Wasteroide)
-    Wplan.delete(Wasteroide1)
-    Wplan.delete(Wasteroide2)
-
     Wasteroide = Wplan.create_image(AsteroX, AsteroY, image=mesImages[NumImage])
-    Wasteroide1 = Wplan.create_image(AsteroX1, AsteroY1, image=mesImages[NumImage])
-    Wasteroide2 = Wplan.create_image(AsteroX2, AsteroY2, image=mesImages[NumImage])
 
     NumImage += 1
     AsteroX -= Deltax
-    AsteroX1-=Deltax
-    AsteroX2-=Deltax
-
     if NumImage==23:
         NumImage=0
         AsteroX=random.randint(750, 1200)
         AsteroY = random.randint(100, 620)
-        AsteroX1 = random.randint(750, 1200)
-        AsteroY1 = random.randint(100, 620)
-        AsteroX2 = random.randint(750, 1200)
-        AsteroY2 = random.randint(100, 620)
     Wplan.after(600 , Attaque)
+
+
+def Bouge(Wimage,letemps):
+    global Wplan
+    Position = Wplan.coords(Wimage)
+    PosX = Position.pop(0)
+    Wplan.coords(Wimage, PosX - 80, Position)
+    time.sleep(1)
+    #    if letemps >500:
+     #       letemps-=10
+  #  Wplan.after(letemps, Attaque)
+
+def chronometre():
+    now = default_timer() - start
+    minutes, seconds = divmod(now, 60)
+    hours, minutes = divmod(minutes, 60)
+    str_time = "%d:%02d:%02d" % (hours, minutes, seconds)
+    Wplan.itemconfigure(chrono, text=str_time, fill='white', font=Times)
+    window.after(1000, chronometre)
 
 
 window=Tk()
 window.title("SPACE JUMPER 3000")
-window.geometry("1280x720+40+50")
 
+# Faire en sorte que la taille de la fenêtre s'adapte à n'importe quel écran :
+largeur_fenetre = window.winfo_screenwidth()
+hauteur_fenetre = window.winfo_screenheight()
+
+window.geometry("%dx%d+0+0" % (largeur_fenetre, hauteur_fenetre))
+#window.geometry("1280x720+40+50")
 
 #------- Police -------#
 
-Times = font.Font(family='Times', size=16, weight='bold')
 
-Ciel=PhotoImage(file='C:/Users/remi1/Desktop/SpaceJUMP/Ciel.png')
-Fusee=PhotoImage(file='C:/Users/remi1/Desktop/SpaceJUMP/fusée2.png')
+Times = font.Font(family='Times', size=18, weight='bold')
+
+Ciel=PhotoImage(file='Ciel.png')
+Fusee=PhotoImage(file='fusée2.png')
 #Asteroide=PhotoImage(file='C:/Users/remi1/Desktop/SpaceJUMP/Astéroïde.png')
 
 #------------- Graphique accueil --------#
 
-Wplan=Canvas(window, width=1280, height=720)
+Wplan=Canvas(window, width=largeur_fenetre, height=hauteur_fenetre)
 Wplan.grid()
 Wplan.create_image(640, 360, image=Ciel)
+AffichageChrono = Wplan.create_text(largeur_fenetre//2,hauteur_fenetre//2)
 Wfusee=Wplan.create_image(100,360, image=Fusee)
+
+#---Affichage Chrono ---#
+start = default_timer()
+chrono = Wplan.create_text(40, 20)
+
 
 mesImages=[]
 for i in range(24):
-    nom = "C:/Users/remi1/Desktop/SpaceJUMP/img/Astéroïde" + str(i) + ".png"
+    nom = "img\Astéroïde" + str(i) + ".png"
     mesImages.append(PhotoImage(file=nom))
 
 #---------------#
-
 AsteroX=random.randint(750, 1200)
 AsteroY=random.randint(100, 620)
-AsteroX1=random.randint(750, 1200)
-AsteroY1=random.randint(100, 620)
-AsteroX2=random.randint(750, 1200)
-AsteroY2=random.randint(100, 620)
-
 Deltax=50
 NumImage=0
 temps=2500
 
 Wasteroide=Wplan.create_image(random.randint(750, 1200), random.randint(100, 620))
-Wasteroide1=Wplan.create_image(random.randint(750, 1200), random.randint(100, 620))
-Wasteroide2=Wplan.create_image(random.randint(750, 1200), random.randint(100, 620))
+
 
 #----------
 Wplan.bind_all('z', Haut)
@@ -136,14 +143,9 @@ Wplan.bind_all('s', Bas)
 Wplan.bind_all('d', Droite)
 Wplan.bind_all('q', Gauche)
 
+
+
+chronometre()
 Attaque()
 
-#JOEKPFOJGNREIGNEPOFJKEJFOZE
-
-while True:
-    window.update_idletasks()
-    window.update()
-    time.sleep(0.01)
-
-
-
+window.mainloop()
