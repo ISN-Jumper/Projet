eb18482e3f971b05b670b5d774a15e9450592584
TheRemi120 <62143635+TheRemi120@users.noreply.github.com>
2020-05-11 12:51:18 +0200

    GROS MENAGE qui m'a pris 1 h :'( + commentaires = wow

--- Accueil.py
+++ Accueil.py
@@ -5,41 +5,27 @@ from tkinter import font
 import os
 from PIL import Image, ImageTk
 import openpyxl
-import time
-from pygame import mixer
-import csv
-
-mixer.init()
-mixer.music.load("DuaLipa.mp3")
-mixer.music.play(-1)
-mixer.music.set_volume(0.2)
 
 def ChangePage():
-    os.startfile("C:/Users/User/Desktop/ProjetTest2/PartieJeu.py")
+    os.startfile("PartieJeu.py") #Redirection vers le fichier contenant le programme contenant le code pour le jeu
     window.quit()
+
 def Exit(evt):
-    window.attributes('-fullscreen', True)
-    Wplan.bind_all('<Escape>', Tixe)
-def Tixe(evt):
+    window.attributes('-fullscreen', True) # lorsque le joueur appuie sur echap le jeu est en plein ecran
+    Wplan.bind_all('<Escape>', Exit_inverse) # si il réappuie sur echap, le jeu redeviendra en fenêtré grâce à la fonction Exit_inverse
+def Exit_inverse(evt):
     window.attributes('-fullscreen', False)
     Wplan.bind_all('<Escape>', Exit)
 
-def FullEcran():
+def FullScreen():    # les fonctions FullEcran sont identiques à "Exit" mais elles sont utilisés à l'aide d'un bouton
     window.attributes('-fullscreen', False)
-    WFullscreen.config(command = lambda : FullEcran2())
-def FullEcran2():
+    WFullscreen.config(command = lambda : FullScreen2())
+def FullScreen2():
     window.attributes('-fullscreen', True)
-    WFullscreen.config(command= lambda : FullEcran())
+    WFullscreen.config(command= lambda : FullScreen())
 
-def Copyrights():
-    os.startfile("C:/Users/User/Desktop/ProjetTest2/Copyrights.py")
-    window.quit()
 
-def Commandes():
-    os.startfile("C:/Users/User/Desktop/ProjetTest2/Commandes.py")
-    window.quit()
-
-def FuseeAnim():
+def FuseeAnim(): #Cette fonction a été conçue pour l'animation lorsque le jeu est lancé
     global Wfusee, deltaX, deltaY, FuseePil, Fusee, Passer, NumImage
 
     if Wplan.coords(Wfusee)[0] < (largeur_fenetre//2)-56 and Wplan.coords(Wfusee)[1] == 360 :
@@ -71,7 +57,7 @@ NumImage=0
 Passer = False
 
 def TitreJumper():
-    Wtitre=Wplan.create_image(largeur_fenetre//2, (hauteur_fenetre//2)-75, image=TkTitre)
+    Wtitre=Wplan.create_image(largeur_fenetre//2, (hauteur_fenetre//2)-75, image=TkTitre) # Une fois la fusée posée, le titre s'affiche
 
 
 if __name__ == '__main__':
@@ -85,14 +71,11 @@ if __name__ == '__main__':
 
     window.geometry("%dx%d+0+0" % (largeur_fenetre,hauteur_fenetre))
     window.attributes('-fullscreen', True)
-    #window.geometry("1280x720+40+50")
 
     #------- Police -------#
 
     Times = font.Font(family='Times', size=24, weight='bold')
     Times2 = font.Font(family='Times', size=16, weight='bold')
-
-    #Ciel=PhotoImage(file='CielAccueil.png')
 
     #------Config Image-----#
 
@@ -103,10 +86,11 @@ if __name__ == '__main__':
     Titre = Image.open('Titre.png')
     TkTitre= ImageTk.PhotoImage(Titre)
 
+    #------- Configuration du score --------#
 
-    #---Lecture du meilleur score sur le CSV---#
-    with open("CSV_Score.csv","r") as ScoreCSV:
-        meilleur_score = eval(ScoreCSV.read())
+    worksheets = openpyxl.load_workbook("Score.xlsx") #Le programme charge le fichier excel contenant le score
+    worksheet = worksheets.active
+    B1 = worksheet.cell(row=1, column=2)     #Le programme lit la cellule B1 contenant le score du joueur et l'affiche avec un widget créé plus bas
 
     #------------- Graphique accueil --------#
 
@@ -118,9 +102,6 @@ if __name__ == '__main__':
     Ciel = ImageTk.PhotoImage(WCCiel)
     WCiel = Wplan.create_image((largeur_fenetre // 2) - 2, (hauteur_fenetre // 2) - 2, image=Ciel)
 
-  #  FuseePil = Image.open("fusée2.png")
- #   FuseePil = FuseePil.rotate(45)
-   # Fusee = ImageTk.PhotoImage(FuseePil)
     deltaX=-20
     deltaY=360
     Wfusee = Wplan.create_image(deltaX, deltaY, image=FuseeFeu)
@@ -140,33 +121,27 @@ if __name__ == '__main__':
     Jouer.grid()
     Jouer.place(x=(largeur_fenetre//2)-100, y=(hauteur_fenetre//1.5))
 
-    Boutique = Button(window, text='Boutique', width=12, font=Times2, command=lambda: ChangePage())
+    Boutique = Button(window, text='Boutique', width=12, font=Times2) #Fonctionnalité à venir
     Boutique.grid()
     Boutique.place(x=(largeur_fenetre // 2) - 86, y=(hauteur_fenetre // 1.35))
 
-    Commandes = Button(window, text='Commandes', width=12, font=Times2, command=lambda: Commandes())
-    Commandes.grid()
-    Commandes.place(x=(largeur_fenetre - 215) , y=(hauteur_fenetre - 200))
-
-
-
-    Wquitter = Button(window, text="Quitter", font=Times2, command=window.quit)
+    Wquitter = Button(window, text="Quitter", font=Times2, command=window.quit) # Bouton permettant de quitter le jeu
     Wquitter.grid()
     Wquitter.place(x=50, y= hauteur_fenetre -150)
 
-    WFullscreen = Button(window, text="Plein Ecran", font=Times2, command=FullEcran)
+    WFullscreen = Button(window, text="Plein Ecran", font=Times2, command=FullScreen) # Bouton permettant d'afficher le jeu en plein ecran
     WFullscreen.grid()
     WFullscreen.place(x=40, y=40)
 
-    WScore = Label(window, text='Meilleur Score : ', font=Times)
-    WScore.place(x=largeur_fenetre - 390, y = 50)
+    #Affichage du score du joueur :
 
-    WPoints = Label(window, text= meilleur_score, font = Times )
+    WScore = Label(window, text='Score : ', font=Times)
+    WScore.place(x=largeur_fenetre - 250, y = 50)
+
+    WPoints = Label(window, text= B1.value, font = Times )
     WPoints.place(x=largeur_fenetre - 125, y = 50)
 
-    WCopyright = Button(window, text='Copyrights', font=Times2, command=lambda: Copyrights())
-    WCopyright.place(x=largeur_fenetre - 200, y=hauteur_fenetre - 150)
-
+    # ---------------------  #
     FuseeAnim()
 
     window.mainloop()
--- PartieJeu.py
+++ PartieJeu.py
@@ -4,177 +4,134 @@ import random
 from timeit import default_timer
 import os
 from PIL import Image, ImageTk
-import time
-from threading import *
-from pygame import mixer
-import csv
 
+#-------------Fonctions-----------#
 
-Nb_Collision = 0
-JeuTermine = False
-
-Niveau1 = 0
-Niveau2 = 0
-Niveau3 = 0
-Niveau4 = 0
-
-
-PremiereCollision = False
-DeuxiemeCollision = False
-TroisiemeCollision = False
-
-mixer.init()
-mixer.music.load("DuaLipa.mp3")
-mixer.music.play(-1)
-mixer.music.set_volume(0.2)
-
-
-# Generate timing
-generateTime = time.time()
-refreshTime = time.time()
-laserTime = time.time()
-
-# New Laser
-new_Laser = False
-
-
+#---- Mouvement fusée ---#
 def Haut(evt):
     global Wplan
     global Wfusee
-    PositionFusee = Wplan.coords(Wfusee)
-    PosY = PositionFusee.pop(1)
-    if PosY -65 > 35:
-        Wplan.coords(Wfusee, PositionFusee, PosY - 65)
+    PosY = Wplan.coords(Wfusee)[1]  #PosY devient la variable contenant les coordonées en Y de la fusée
+    if PosY -50 > 50: #On empêche la fusée de sortir du cadre
+            # Si la position de la fusée en Y -50 ( on retire 50 car la fusée à une largeur de 100 ) est plus grande que 50 alors :
+        Wplan.coords(Wfusee, Wplan.coords(Wfusee)[0], PosY -50)
     else:
-        Wplan.coords(Wfusee, PositionFusee, PosY)
-
+        Wplan.coords(Wfusee, Wplan.coords(Wfusee)[0], PosY)
 def Bas(evt):
     global Wplan
     global Wfusee
-    PositionFusee = Wplan.coords(Wfusee)
-    PosY = PositionFusee.pop(1)
-    if PosY +65 < 705:  # L'axe des ordonnées est dirigé vers le bas
-        Wplan.coords(Wfusee, PositionFusee, PosY + 65)
+    PosY = Wplan.coords(Wfusee)[1]
+    if PosY +83 < hauteur_fenetre:  # la fusée a une largeur de 175 pixel, d'où le + 83 (175//2)
+                            #Si sa position en Y est inférieur à la hauteur de la fenêtre alors :
+        Wplan.coords(Wfusee, Wplan.coords(Wfusee)[0], PosY + 65)
     else:
-        Wplan.coords(Wfusee, PositionFusee, PosY)
-
+        Wplan.coords(Wfusee, Wplan.coords(Wfusee)[0], PosY) # Sinon la fusée ne descend pas plus bas
 def Gauche(evt):
     global Wplan
     global Wfusee
-    PositionFusee = Wplan.coords(Wfusee)
-    PosX = PositionFusee.pop(0)
-    if PosX - 30 > 100:  # L'axe des ordonnées est dirigé vers le bas
-        Wplan.coords(Wfusee, PosX - 30, PositionFusee)
+    PosX = Wplan.coords(Wfusee)[0] #PosX devient la variable contenant les coordonées en X de la fusée
+    if PosX - 83 > 0:
+        Wplan.coords(Wfusee, PosX - 30, Wplan.coords(Wfusee)[1])
     else:
-        Wplan.coords(Wfusee, PosX, PositionFusee)
-
+        Wplan.coords(Wfusee, PosX, Wplan.coords(Wfusee)[1])
 def Droite(evt):
     global Wplan
     global Wfusee
 
-    PositionFusee = Wplan.coords(Wfusee)
-    PosX = PositionFusee.pop(0)
-    if PosX + 30 < 400:  # L'axe des ordonnées est dirigé vers le bas
-        Wplan.coords(Wfusee, PosX +30, PositionFusee)
+    PosX=Wplan.coords(Wfusee)[0]
+    if PosX + 30 < 400:  #Délimitation en X à 400 de manière à ce que la fusée ne se déplace pas là où les astéroides spawn
+
+        Wplan.coords(Wfusee, PosX +30, Wplan.coords(Wfusee)[1])
     else:
-        Wplan.coords(Wfusee, PosX, PositionFusee)
+        Wplan.coords(Wfusee, PosX, Wplan.coords(Wfusee)[1])
 
-def Exit(evt):
-    window.attributes('-fullscreen', True)
-    Wplan.bind_all('<Escape>', Tixe)
-def Tixe(evt):
-    window.attributes('-fullscreen', False)
-    Wplan.bind_all('<Escape>', Exit)
+#-----Generer Asteroide------#
+class Attaque_asteroide :
+    def __init__(self, AsteroX,AsteroY, Wplan):
 
+        self.Wplan = Wplan
+        self.id = self.Wplan.create_image(AsteroX, AsteroY, image=mesImages[NumImage]) #grâce à cette class, chaque asteroide est identifiable avec self.id
+        self.tour=0
+    def animation_2(self):
+        global Wfusee
+        global Barre
+        global Wplan
+        global PremiereCollision, DeuxiemeCollision
+        global NumImage, mesImages, Wplan, tour
 
+        if Wplan.coords(self.id)[0] > -50 :
+            self.Wplan.move(self.id, -23, 0)
+            self.tour+=23
+            NumImage += 1
+            self.Wplan.itemconfig(self.id, image=mesImages[NumImage])
+            if self.tour >= largeur_fenetre :
+                self.tour=0
+                self.Wplan.delete(self.id)
+
+            if NumImage == 23:
+                NumImage = 0
+            if (Wplan.coords(Wfusee)[0]) + 72.5 >= (Wplan.coords(self.id)[0]) - 50 and (Wplan.coords(Wfusee)[0]) - 72.5 <= (Wplan.coords(self.id)[0]) + 50 :
+                if (Wplan.coords(Wfusee)[1]) + 25 >= (Wplan.coords(self.id)[1]) - 50 > (Wplan.coords(Wfusee)[1]) - 25 or (Wplan.coords(Wfusee)[1]) - 25 <= (Wplan.coords(self.id)[1]) + 50 < (Wplan.coords(Wfusee)[1]) + 25 or(Wplan.coords(self.id)[1]) + 50 >= (Wplan.coords(Wfusee)[1]) + 25 > (Wplan.coords(Wfusee)[1]) - 25 >= (Wplan.coords(self.id)[1]) - 50:
+                    Wplan.itemconfig(self.id, image=Invisible)
+                    Wplan.coords(self.id, Wplan.coords(self.id)[0] -460, Wplan.coords(self.id)[1])
+                    if PremiereCollision == False:
+                        Wplan.itemconfig(Barre, image=DeuxCarre)
+                        PremiereCollision = True
+                    elif DeuxiemeCollision == False:
+                        Wplan.itemconfig(Barre, image=UnCarre)
+                        DeuxiemeCollision = True
+                    elif DeuxiemeCollision == True:
+                        os.startfile("C:/Users/remi1/PycharmProjects/SpaceJumper3000/GameOverPage.py")
+                        window.quit()
+def Create_asteroide():
+    global Temps_Generer, NumImage, mesImages
+
+    if Temps_Generer > 900 : #Si le temps de génération entre deux asteroides est supérieur à 900
+        Temps_Generer-=100  #Alors le temps diminue de 100 ms
+    AsteroX = random.randint(1000, largeur_fenetre-50) #Coordonées en X de l'asteroide générés aléatoirement entre 1000 et la largeur de la fenêtre -50 car la largeur de l'asteroide est de 100
+    AsteroY = random.randint(50, hauteur_fenetre-50) #Pareillement pour les coordonées en Y
+    Wasteroide = Attaque_asteroide(AsteroX, AsteroY, Wplan) #Creation de l'asteroide grâce à la class Attaque_asteroide
+    EnsembleAsteroide.add(Wasteroide)   #On repertorie l'asteroide dans le set EnsembleAsteroide
+
+    window.after(Temps_Generer, Create_asteroide) #Après le temps de generation entre deux asteroide on recréer un nouvel asteroide
+def animation_asteroide():
+    for Wasteroide in EnsembleAsteroide: #Pour les asteroides dans le set EnsembleAsteroide
+        Wasteroide.animation_2() #on utilise la fonction animation_2 dans la class Attaque_asteroide pour les animer
+    window.after(350, animation_asteroide)
+
+#--------Generer Laser -------#
 class Perso:
     def __init__(self, Cordox, Cordoy, Wplan):
         self.Wplan = Wplan
         self.id = self.Wplan.create_image(Cordox, Cordoy, image=Laser)
 
     def animation(self):
-        global NbrePoints
-        global Wplan
-        global NbreTentative
-
         self.Wplan.move(self.id, 6, 0)
-
-
-NbreTentative = 0
-
-
 def Create_laser(event):
-    global personnage, new_Laser
-    new_Laser = True
-    MusiqueLaser = mixer.Sound("BruitageLaserWav.wav")
-    MusiqueLaser.set_volume(0.1)
-    MusiqueLaser.play()
-
-
-
-
-def animation_collsion_laser():
-    global NbreTentative
-    global NumImage
-    global NbrePoints
-    global laserTime, new_Laser
-    actualTime = time.time()
-
-    if actualTime >= laserTime +.003:
-        laserTime = actualTime
-
-        if new_Laser:
-            Cordo = Wplan.coords(Wfusee)
-            LaserX = Cordo.pop(0) + 87.5 + 10
-            LaserY = Cordo.pop(0)
-            personnage = Perso(LaserX, LaserY, Wplan)
-            EnsembleLaser.append(personnage)
-            Bruitage = mixer.Sound("BruitageLaserWav.wav")
-            Bruitage.set_volume(0.1)
-            Bruitage.play()
-            new_Laser = False
-
-        toPopPerso = []
-        for personnage in EnsembleLaser:
-            personnage.animation()
-            x0 = personnage.id
-            x0 = Wplan.coords(personnage.id)[0]
-            if x0 > largeur_fenetre - 1:
-                toPopPerso.append(personnage)
-
-            toPopAstero = []
-            for Astero in dicoAstero:
-                if (Wplan.coords(personnage.id)[0]) + 20 >= (Wplan.coords(Astero)[0]) - 50 and (
-                Wplan.coords(personnage.id)[0]) - 20 <= (Wplan.coords(Astero)[0]) + 50:
-                    if (Wplan.coords(personnage.id)[1]) + 1.75 >= (Wplan.coords(Astero)[1]) - 50 > (Wplan.coords(personnage.id)[1]) - 1.75 or (Wplan.coords(personnage.id)[1]) - 1.75 <= (Wplan.coords(Astero)[1]) + 50 < (Wplan.coords(personnage.id)[1]) + 1.75 or (Wplan.coords(Astero)[1]) + 50 >= (Wplan.coords(personnage.id)[1]) + 1.75 > (Wplan.coords(personnage.id)[1]) - 1.75 >= (Wplan.coords(Astero)[1]) - 50:
-                        # Wplan.coords(personnage.id, largeur_fenetre - 10, Wplan.coords(personnage.id)[1])
-                        Explosion = mixer.Sound("BruitageCraquement.wav")
-                        Explosion.set_volume(0.1)
-                        Explosion.play()
-                        NbreTentative += 1
-                        Wplan.itemconfig(personnage.id, image=Invisible)
-                        Wplan.coords(personnage.id, largeur_fenetre - 10, Wplan.coords(personnage.id)[1])
-                        print("NbreTentative",NbreTentative)
-                        if NbreTentative >= 3:
-                            AsteroideDetruit = mixer.Sound("AsteroideDetruit.wav")
-                            AsteroideDetruit.set_volume(0.5)
-                            AsteroideDetruit.play()
-                            Wplan.itemconfig(personnage.id, image=Invisible)
-                            Wplan.coords(personnage.id, largeur_fenetre - 10, Wplan.coords(personnage.id)[1])
-                            toPopAstero.append(Astero)
-                            NbreTentative = 0
-                            NbrePoints += 1
-                            Wplan.itemconfig(CanvasPoints, text=('Nombre de Points :', NbrePoints))
-            for Astero in toPopAstero:
-                Wplan.delete(Astero)
-                dicoAstero.pop(Astero)
-        for personnage in toPopPerso:
+    Cordo = Wplan.coords(Wfusee)
+    LaserX = Cordo.pop(0) + 87.5
+    LaserY = Cordo.pop(0)
+    personnage = Perso(LaserX, LaserY, Wplan)
+    EnsembleLaser.add(personnage)
+def animation_laser():
+    for personnage in EnsembleLaser:
+        personnage.animation()
+        x0 = personnage.id
+        if x0 > largeur_fenetre - 1:
             Wplan.delete(personnage.id)
             EnsembleLaser.remove(personnage)
+    window.after(3, animation_laser)
 
+#-------FullScreen-------#
+def Exit(evt): #Même fonctions utilisées pour l'accueil
+    window.attributes('-fullscreen', True)
+    Wplan.bind_all('<Escape>', Exit_inverse)
+def Exit_inverse(evt):
+    window.attributes('-fullscreen', False)
+    Wplan.bind_all('<Escape>', Exit)
+#----
 
 def chronometre():
-    global str_time
     now = default_timer() - start
     minutes, seconds = divmod(now, 60)
     hours, minutes = divmod(minutes, 60)
@@ -182,134 +139,10 @@ def chronometre():
     Wplan.itemconfigure(chrono, text=str_time, fill='white', font=Times)
     window.after(1000, chronometre)
 
-str_time ="%d:%02d:%02d"
-
-
-class Principale(Thread):
-    def __init__(self):
-        Thread.__init__(self)
-        self.start()
-    def run(self):
-        global NombreDeVie
-        global Wplan
-        global Wfusee, Astero, dicoAstero
-        global str_time
-
-        Vivant = True
-        while Vivant:
-            GenererAstero()
-            animation_collsion_laser()
-            DetectionCollision()
-            SuppressionAstero()
-            Scores()
-        time.sleep(1)
-
-
-def GenererAstero():
-    global mesImages
-    global Wplan
-    global generateTime, refreshTime
-    if Vivant:
-        actualTime = time.time()
-        if actualTime >= generateTime + 1:
-            generateTime = actualTime
-            AsteroX = random.randint(800, 1200)
-            AsteroY = random.randint(100, 620)
-            Astero = Wplan.create_image(AsteroX, AsteroY, image=mesImages[0])
-            dicoAstero[Astero] = [AsteroX, AsteroY, 0]
-
-        if actualTime >= refreshTime +0.35:
-            refreshTime = actualTime
-            for Astero in dicoAstero:
-                if dicoAstero[Astero][2] == 23:
-                    AsteroX = random.randint(750, 1200)
-                    AsteroY = random.randint(100, 620)
-                    dicoAstero[Astero] = [AsteroX, AsteroY, 0]
-
-                Wplan.move(Astero, -Deltax, 0)
-                Wplan.itemconfigure(Astero, image=mesImages[dicoAstero[Astero][2]])
-                dicoAstero[Astero][2] += 1
-                dicoAstero[Astero][0] -= Deltax
-
-def SuppressionAstero():
-    global Astero, dicoAstero
-
-    listSuppressionAstero = []
-    for Astero in dicoAstero:
-        if dicoAstero[Astero][0] <= 0:
-            listSuppressionAstero.append(Astero)
-    for Astero in listSuppressionAstero:
-        Wplan.delete(Astero)
-        dicoAstero.pop(Astero)
-        print("Astéroide Supprimé")
-
-
-
-
-def DetectionCollision():
-    global Astero, Wfusee, dicoAstero
-    global Wplan
-    global Vivant
-    global mesImages, NumImage
-    global listeScore
-    global NbrePoints
-    global JeuTermine
-
-    global PremiereCollision, DeuxiemeCollision, TroisiemeCollision
-
-    toPopAsteroDetruit = []
-
-    if Vivant == True:
-        for Astero in dicoAstero:
-            if (Wplan.coords(Wfusee)[0]) + 87.5 >= (Wplan.coords(Astero)[0]) - 50 and (Wplan.coords(Wfusee)[0]) - 87.5 <= (Wplan.coords(Astero)[0]) + 50 :
-                if (Wplan.coords(Wfusee)[1]) + 25 >= (Wplan.coords(Astero)[1]) - 50 > (Wplan.coords(Wfusee)[1]) - 25 or (Wplan.coords(Wfusee)[1]) - 25 <= (Wplan.coords(Astero)[1]) + 50 < (Wplan.coords(Wfusee)[1]) + 25 or(Wplan.coords(Astero)[1]) + 50 >= (Wplan.coords(Wfusee)[1]) + 25 > (Wplan.coords(Wfusee)[1]) - 25 >= (Wplan.coords(Astero)[1]) - 50:
-
-                    print("Coordonnées Astéro : ", Wplan.coords(Astero))
-                    print("Coordonnées Fusée : ", Wplan.coords(Wfusee))
-                    toPopAsteroDetruit.append(Astero)
-                    if PremiereCollision == False:
-                        Wplan.itemconfig(Barre, image=DeuxCarre)
-                        PremiereCollision = True
-                        time.sleep(0.1)
-                    elif DeuxiemeCollision == False:
-                        Wplan.itemconfig(Barre, image=UnCarre)
-                        DeuxiemeCollision = True
-                        time.sleep(0.1)
-                    elif DeuxiemeCollision == True:
-                        Wplan.itemconfig(Barre, image=GameOver)
-                        os.startfile("GameOverPage.py")
-                        window.quit()
-        for Astero in toPopAsteroDetruit:
-            Wplan.delete(Astero)
-            dicoAstero.pop(Astero)
-
-
-
-"""
-with open(CSV, 'w') as points:
-    score = csv.writer(points, delimiter=',')
-    score.writerow(listeScore)
-else:
-listeScore[score] = listeScore[score]
-"""
-
-def Scores():
-    global NbrePoints
-
-    with open("CSV_Score.csv","r") as ScoreCSV:
-        scores = eval(ScoreCSV.read())
-        print("CSV lu")
-
-        if NbrePoints > scores:
-            with open("CSV_Score.csv", "w") as ScoreCSV:
-                ScoreCSV.write(str(NbrePoints))
-                print("Nombres de points = ", NbrePoints)
-                print("Remplacement meilleur score")
-
+#---------------------------------#
 
 window = Tk()
 window.title("SPACE JUMPER 3000")
-
 
 # Faire en sorte que la taille de la fenêtre s'adapte à n'importe quel écran :
 largeur_fenetre = window.winfo_screenwidth()
@@ -319,70 +152,59 @@ window.geometry("%dx%d+0+0" % (largeur_fenetre, hauteur_fenetre))
 window.attributes('-fullscreen', True)
 
 #------- Police -------#
-
 Times = font.Font(family='Times', size=18, weight='bold')
 
-EnsembleLaser=[]
-EnsembleAsteroide=[]
-Temps_Generer=5000
+#------ Variable fonction create_laser et animation_laser -------#
 
-#window.after(10, animation_asteroide)
+EnsembleLaser=set() #Initialisation d'un set contenant les lasers créés par l'utilisateur
+
+#------ Variables fonction create_asteroide et animation_asteroide -------#
+
+EnsembleAsteroide=set() #Initialisation d'un set contenant les astéroides créés par le programme
+Temps_Generer=5000 # Temps de génération entre les deux premiers asteroides de  5 secondes (diminue dans le temps)
+
+# --------- #
+animation_laser()
+animation_asteroide()
+
 #---------Image-------#
-
 Fusee=PhotoImage(file='Fusées/Fusee0.png')
 Laser=PhotoImage(file='Laser.png')
 Invisible=PhotoImage(file='invisible.png')
 
-#---Test---#
-personnage =""
 #------------- Graphique accueil --------#
-
 Wplan=Canvas(window, width=largeur_fenetre, height=hauteur_fenetre)
 Wplan.grid()
 
+#------ Fond --------- #
 WCCiel=Image.open('Ciel.png')
-WCCiel = WCCiel.resize((largeur_fenetre, hauteur_fenetre), Image.ANTIALIAS)
-Ciel=ImageTk.PhotoImage(WCCiel)
-WCiel=Wplan.create_image((largeur_fenetre//2)-2, (hauteur_fenetre//2)-2, image=Ciel)
+WCCiel = WCCiel.resize((largeur_fenetre, hauteur_fenetre), Image.ANTIALIAS) # La taille du fond s'adapte à la taille de l'ecran grâce à la bibliothèque PIL
+Ciel=ImageTk.PhotoImage(WCCiel)   #Configuration de l'image pour tkinter
+WCiel=Wplan.create_image((largeur_fenetre//2)-2, (hauteur_fenetre//2)-2, image=Ciel) #Creation de l'image dans le canvas
 
+#-------#
 AffichageChrono = Wplan.create_text(largeur_fenetre//2,hauteur_fenetre//2)
 
-Wfusee=Wplan.create_image(100,360, image=Fusee)
+#------#
+Wfusee=Wplan.create_image(100,360, image=Fusee) # Affichage de la fusée
 
-Astero = Wplan.create_image(random.randint(750, 1200), random.randint(100, 620))
-
-mesImages=[]
+#---- Variables pour animation des astéroides ---#
+mesImages=[] # Liste qui contient les positions de l'asteroide et servira a animer l'asteroide
 for i in range(24):
-    nom = "img/Asteroide" + str(i) + ".png"
+    nom = "img\Astéroïde" + str(i) + ".png"
     mesImages.append(PhotoImage(file=nom))
-
+NumImage=0
 
 #---Affichage Chrono ---#
 start = default_timer()
 chrono = Wplan.create_text(40, 20)
-
-#---------------#
-
-Deltax=50
-NumImage=0
-temps=2500
-
-#--- Données importantes pour Astéroïdes qu'on va générer---#
-dicoAstero = {}
-
-#---Gestion CSV---#
-CSV='CSV_Score.csv'
-listeScore = [0]
-MeilleurScore = listeScore[0]
-
-
 
 #--Nombres de Points---#
 NbrePoints = 0
 CanvasPoints = Wplan.create_text(largeur_fenetre/2, 20, text=('Nombre de Points :', NbrePoints), fill='white', font=Times)
 
 #-----Déplacement fusée -----#
-Wplan.bind_all('z', Haut)
+Wplan.bind_all('z', Haut) # Lorsque l'utilisateur appuie sur la touche z (minuscule) la fonction Haut s'active, la fusée monte
 Wplan.bind_all('s', Bas)
 Wplan.bind_all('d', Droite)
 Wplan.bind_all('q', Gauche)
@@ -400,14 +222,12 @@ DeuxCarre=PhotoImage(file='img/Barre de Progression/2.png')
 UnCarre=PhotoImage(file='img/Barre de Progression/1.png')
 GameOver=PhotoImage(file='img/Barre de Progression/Game Over.png')
 
-#---MusiqueDeFond---#
-Vivant=True
+#--- Variables pour collision, fonction animation_asteroide
+PremiereCollision = False #--- Variables booleennes qui serviront à faire baisser la barre de vie du joueur lors d'une collision avec un asteroide
+DeuxiemeCollision = False
 
-Principale()
+#------
 chronometre()
+Create_asteroide()
 
-
-print("MeilleurScore =", listeScore[0])
-
-print(f'{dicoAstero}')
 window.mainloop()
